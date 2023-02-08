bec7c7a1ce9320f4e50a811a5a8f0073a080658f
unknown <ghekghek2@gmail.com>
2023-02-08 15:18:19 +0800

    update feb 8

--- howidesign.js
+++ howidesign.js
@@ -1,85 +1,85 @@
-gsap.registerPlugin(ScrollTrigger);
-let mm = gsap.matchMedia();
+// gsap.registerPlugin(ScrollTrigger);
+// let mm = gsap.matchMedia();
 
-const designItem = gsap.utils.toArray('.item-wrapper')
+// const designItem = gsap.utils.toArray('.item-wrapper')
 
-// gsap.to(".design-image",{
+// // gsap.to(".design-image",{
   
-//     duration:8,
-//     scrollTrigger:{
-//         trigger:".design-image",
-//         start:"top 30%",
-//         end:".+=4900s",
-//         scrub:1,
-//         toggleAction:"restart none none none",
-//         pin:".design-image",
-//         pinSpacing:false,
+// //     duration:8,
+// //     scrollTrigger:{
+// //         trigger:".design-image",
+// //         start:"top 30%",
+// //         end:".+=4900s",
+// //         scrub:1,
+// //         toggleAction:"restart none none none",
+// //         pin:".design-image",
+// //         pinSpacing:false,
  
-//        }
-// })
+// //        }
+// // })
 
 
-designItem.forEach(function (item,index) {
+// designItem.forEach(function (item,index) {
 
  
  
-  mm.add({
-isMobile:"(max-width:500px)",
-isDesktop:"(min-width:500px)"
-  },(context) => {
-let {isMobile,isDesktop} = context.conditions;
+//   mm.add({
+// isMobile:"(max-width:500px)",
+// isDesktop:"(min-width:500px)"
+//   },(context) => {
+// let {isMobile,isDesktop} = context.conditions;
 
-if( index % 2 !== 0){
-  TweenMax.to(item,{
+// if( index % 2 !== 0){
+//   TweenMax.to(item,{
    
-       x:isDesktop ? -100 : 1,
-         duration:0.5,
-         scale: isMobile ? 1: 1.2,
+//        x:isDesktop ? -100 : 1,
+//          duration:0.5,
+//          scale: isMobile ? 1: 1.2,
 
-         ease:Power1.easeinout,
-             scrollTrigger:{
-                 trigger:item,
-                 start:"top center",
-                 end:"bottom center",
-                 toggleActions:"restart reverse restart reverse",
-                 pinSpacing:false,
-                //  markers:true,
-                }
+//          ease:Power1.easeinout,
+//              scrollTrigger:{
+//                  trigger:item,
+//                  start:"top center",
+//                  end:"bottom center",
+//                  toggleActions:"restart reverse restart reverse",
+//                  pinSpacing:false,
+//                 //  markers:true,
+//                 }
        
        
        
        
        
        
-         });
-     }
-else{
-TweenMax.to(item,{
+//          });
+//      }
+// else{
+// TweenMax.to(item,{
    
 
-   duration:0.5,
-   scale: isMobile ? 1: 1.2,
-   x:isDesktop ? 100 : 0,
-   ease:Power1.easeinout,
-       scrollTrigger:{
-           trigger:item,
-           start:"top center",
-           end:"bottom center",
-           toggleActions:"restart reverse restart reverse",
-           pinSpacing:false,
-          //  markers:true,
-          }
+//    duration:0.5,
+//    scale: isMobile ? 1: 1.2,
+//    x:isDesktop ? 100 : 0,
+//    ease:Power1.easeinout,
+//        scrollTrigger:{
+//            trigger:item,
+//            start:"top center",
+//            end:"bottom center",
+//            toggleActions:"restart reverse restart reverse",
+//            pinSpacing:false,
+//           //  markers:true,
+//           }
  
  
  
          
           
  
-   });
+//    });
   
  
-}
-  })
+// }
+//   })
  
 
 
@@ -94,184 +94,26 @@ TweenMax.to(item,{
 
 
 
+// });
+
+
+
+
+
+
+$(window).on('scroll', function(){
+  function isScrollIntoView(elem, index) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(window).height()*.5;
+    if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
+      $(elem).addClass('active');
+    }
+    if(!(elemBottom <= docViewBottom)) {
+      $(elem).removeClass('active');
+    }
+  }
+  var timeline = $('.vertical-scrollable-timeline li');
+  Array.from(timeline).forEach(isScrollIntoView);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// gsap.to(".stick1",{
-
-//   opacity:1,
-//   duration:0.5,
-  
-//   y:200,
-//   ease:Power4.easein,
-//       scrollTrigger:{
-//           trigger:".pin1",
-//           start:"top 0",
-//           end:"bottom 0",
-//           toggleActions:"restart none none reverse",
-//           pinSpacing:true,
-//           markers:true,
-//          }
-
-// })
-// gsap.to(".stick2",{
-
-//   opacity:1,
-//   duration:0.5,
-  
-//   y:200,
-//   ease:Power4.easein,
-//       scrollTrigger:{
-//           trigger:".pin2",
-//           start:"top 30%",
-//           end:"bottom 0",
-//           toggleActions:"restart none none reverse",
-//           pinSpacing:true,
-//           markers:true,
-//          }
-
-// })
-// gsap.to(".stick3",{
-
-//   opacity:1,
-//   duration:0.5,
-  
-//   y:200,
-//   ease:Power4.easein,
-//       scrollTrigger:{
-//           trigger:".pin3",
-//           start:"top 0",
-//           end:"bottom 0",
-//           toggleActions:"restart none none reverse",
-//           pinSpacing:true,
-//           markers:true,
-//          }
-
-// })
-// gsap.to(".stick4",{
-
-//   opacity:1,
-//   duration:0.5,
-  
-//   y:200,
-//   ease:Power4.easein,
-//       scrollTrigger:{
-//           trigger:".pin4",
-//           start:"top 0",
-//           end:"bottom 0",
-//           toggleActions:"restart none none reverse",
-//           pinSpacing:true,
-//           markers:true,
-//          }
-
-// })
-// gsap.to(".stick5",{
-
-//   opacity:1,
-//   duration:0.5,
-  
-//   y:200,
-//   ease:Power4.easein,
-//       scrollTrigger:{
-//           trigger:".pin5",
-//           start:"top 0",
-//           end:"bottom 0",
-//           toggleActions:"restart none none reverse",
-//           pinSpacing:true,
-//           markers:true,
-//          }
-
-// })
-// gsap.to(".stick6",{
-
-//   opacity:1,
-//   duration:0.5,
-  
-//   y:200,
-//   ease:Power4.easein,
-//       scrollTrigger:{
-//           trigger:".pin6",
-//           start:"top 0",
-//           end:"bottom 0",
-//           toggleActions:"restart none none reverse",
-//           pinSpacing:true,
-//           markers:true,
-//          }
-
-// })
-// gsap.to(".stick7",{
-
-//   opacity:1,
-//   duration:0.5,
-  
-//   y:200,
-//   ease:Power4.easein,
-//       scrollTrigger:{
-//           trigger:".pin7",
-//           start:"top 0",
-//           end:"+=500s",
-//           toggleActions:"restart none none reverse",
-//           pinSpacing:true,
-//           markers:true,
-//          }
-
-// })
-
-
-
-// gsap.to(".square1",{
-//  background:"url(img/Group102.png)",
-
-//     duration:8,
-//     scrollTrigger:{
-//         trigger:".square3",
-      
-//         scrub:1,
-//         start:"top 50%",
-//       end:"top 50%",
-//         pinSpacing:false,
-//         markers:true,
-//        }
-// })
-// gsap.to(".square1",{
-  
-//     duration:20,
-//     scrollTrigger:{
-//         trigger:".square3",
-//         start:"top 50%",
-//         end:".square3",
-//         scrub:1,
-//         toggleAction:"restart none none none",
-//         pin:".square1",
-//         pinSpacing:false,
-//         markers:true,
-//        }
-// })
-// gsap.to(".square1",{
-  
-//     duration:20,
-//     scrollTrigger:{
-//         trigger:".square4",
-//         start:"top 50%",
-//         end:"+=100",
-//         scrub:1,
-//         toggleAction:"restart none none none",
-//         pin:".square1",
-//         pinSpacing:false,
-//         markers:true,
-//        }
-// })
--- work.js
+++ work.js
@@ -1,48 +1,44 @@
-gsap.to(".work-details",{
+let gtl = gsap.timeline({defaults:{ease:"SlowMo.easeOut"}})
+
+// gsap.to(".work-details",{
   
-    duration:8,
-    scrollTrigger:{
-        trigger:".work-details",
-        start:"top 10%",
-        end:"bottom 100%",
+//     duration:8,
+//     scrollTrigger:{
+//         trigger:".work-details",
+//         start:"top 10%",
+//         end:"bottom 100%",
     
-        scrub:1,
-        toggleAction:"restart none none none",
-        pin:".work-details",
-        pinSpacing:false,
-        markers:true,
+//         scrub:1,
+//         toggleAction:"restart none none none",
+//         pin:".work-details",
+//         pinSpacing:false,
+//         markers:true,
  
-       }
-})
+//        }
+// })
 
 let ghome =  document.querySelector("#ghome")
 let gservices =  document.querySelector("#gservices")
 
-ghome.addEventListener("click", function(){ 
+
     
-    gsap.from(
-        ".homefix-container > *",
-        {
-          y: 100,
-          ease: "Power2.easeInOut",
-          opacity: 0,
-          duration: 0.8,
-          stagger: 0.5,
-        }
-      );
-    gsap.from(
-        ".brand-container > *",
-        {
-          y: 200,
-          ease: "Power2.easeInOut",
-          opacity: 0,
-          duration: 0.8,
-          stagger: 0.3,
-        }
-      );
+var isMobile = window.innerWidth <= 500;
+
+if (!isMobile) {
+    gsap.to(".work-details",{
   
-
-
-
-}); 
-
+        duration:8,
+        scrollTrigger:{
+            trigger:".work-details",
+            start:"top 10%",
+            end:"bottom 100%",
+        
+            scrub:1,
+            toggleAction:"restart none none none",
+            pin:".work-details",
+            pinSpacing:false,
+            markers:true,
+     
+           }
+    })
+  }
